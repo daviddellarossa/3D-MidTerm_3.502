9fabc742d35a9d2d665898f173437cb1931fcf03
David Della Rossa <david.della.rossa@unity3d.com>
2021-06-08 08:15:01 +0100

    State machine added to Player

--- 3.502 Interactive Physics Scene/Assets/Scripts/MouseManager.cs
+++ 3.502 Interactive Physics Scene/Assets/Scripts/MouseManager.cs
@@ -2,19 +2,28 @@
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class MouseManager : MonoBehaviour
 {
-    //Billiard stick icon from https://depositphotos.com/vector-images/cue.html
     //Mouse icon from https://freeiconshop.com/icon/cursor-icon-outline/
 
-    public Texture2D MouseDefault;
-    public Texture2D MousePlayer;
+    [SerializeField] private State CurrentState;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetState(IdleState.Instance);
+    }
+
+    private void SetState(State state)
+    {
+        if (CurrentState != null)
+        {
+            CurrentState.OnExit();
+        }
+        CurrentState = state;
+        CurrentState.OnEnter();
     }
 
     // Update is called once per frame
@@ -45,5 +54,112 @@ public class MouseManager : MonoBehaviour
          *      When the ball position is equal to or greater than the initial position, destroy the Spring force and change state to Idle
          * 
          */
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Left Mouse Button clicked");
+        }
+    }
+
+    private abstract class State
+    {
+        public abstract void OnEnter();
+        public abstract void OnExit();
+    }
+
+    private class IdleState : State
+    {
+        private static IdleState instance;
+
+        public static IdleState Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new IdleState();
+                }
+
+                return instance;
+            }
+        }
+
+        public override void OnEnter()
+        {
+            Debug.Log("Entering Idle State");
+        }
+
+        public override void OnExit()
+        {
+            Debug.Log("Exiting Idle State");
+        }
+
+        private IdleState()
+        {
+            
+        }
+    }
+
+    private class ChargeState : State
+    {
+        private static ChargeState instance;
+        public static ChargeState Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ChargeState();
+                }
+
+                return instance;
+            }
+        }
+
+        public override void OnEnter()
+        {
+            Debug.Log("Entering Charge State");
+        }
+
+        public override void OnExit()
+        {
+            Debug.Log("Exiting Charge State");
+        }
+        private ChargeState()
+        {
+            
+        }
+    }
+
+    private class ReleaseState : State
+    {
+        private static ReleaseState instance;
+        public static ReleaseState Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ReleaseState();
+                }
+
+                return instance;
+            }
+        }
+
+        public override void OnEnter()
+        {
+            Debug.Log("Entering Release State");
+        }
+
+        public override void OnExit()
+        {
+            Debug.Log("Exiting Release State");
+        }
+
+        private ReleaseState()
+        {
+            
+        }
     }
 }
--- 3.502 Interactive Physics Scene/Assets/Scripts/Player.cs
+++ 3.502 Interactive Physics Scene/Assets/Scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +8,8 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
 
     public Texture2D MousePlayer;
+
+    [SerializeField] private State CurrentState;
 
     void OnMouseEnter()
     {
@@ -18,4 +21,217 @@ public class Player : MonoBehaviour
     {
         Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
     }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Mouse Button clicked on Player");
+        CurrentState.OnMouseDown();
+    }
+
+    void OnMouseUp()
+    {
+        CurrentState.OnMouseUp();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetState(IdleState.Instance);
+    }
+
+    void Update()
+    {
+        //if (Input.GetMouseButtonDown(1))
+        //{
+        //    Debug.Log("Right mouse button down");
+        //}
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Escape button down");
+            CurrentState.OnKeyPressed(KeyCode.Escape);
+        }
+    }
+
+    private void SetState(State state)
+    {
+        if (CurrentState != null)
+        {
+            CurrentState.OnExit();
+            CurrentState.ChangeState -= CurrentState_ChangeState;
+
+        }
+        CurrentState = state;
+        CurrentState.ChangeState += CurrentState_ChangeState;
+        CurrentState.OnEnter();
+    }
+
+    private void CurrentState_ChangeState(object sender, State e)
+    {
+        Debug.Log("ChangeState event intercepted");
+        SetState(e);
+    }
+
+    private abstract class State
+    {
+        public abstract event EventHandler<State> ChangeState;
+        public abstract void OnEnter();
+        public abstract void OnExit();
+        public abstract void OnMouseDown();
+        public abstract void OnMouseUp();
+        public abstract void OnKeyPressed(KeyCode keyCode);
+    }
+
+    private class IdleState : State
+    {
+        private static IdleState instance;
+
+        public static IdleState Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new IdleState();
+                }
+
+                return instance;
+            }
+        }
+
+        public override event EventHandler<State> ChangeState;
+
+        public override void OnEnter()
+        {
+            Debug.Log("Entering Idle State");
+        }
+
+        public override void OnExit()
+        {
+            Debug.Log("Exiting Idle State");
+        }
+
+        public override void OnMouseDown()
+        {
+            Debug.Log("Idle State: Mouse Down");
+            ChangeState?.Invoke(this, ChargeState.Instance);
+        }
+
+        public override void OnMouseUp()
+        {
+        }
+
+        public override void OnKeyPressed(KeyCode keyCode)
+        {
+        }
+
+        private IdleState()
+        {
+
+        }
+    }
+
+    private class ChargeState : State
+    {
+        private static ChargeState instance;
+        public static ChargeState Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ChargeState();
+                }
+
+                return instance;
+            }
+        }
+
+        public override event EventHandler<State> ChangeState;
+
+        public override void OnEnter()
+        {
+            Debug.Log("Entering Charge State");
+        }
+
+        public override void OnExit()
+        {
+            Debug.Log("Exiting Charge State");
+        }
+        public override void OnMouseDown()
+        {
+        }
+
+        public override void OnMouseUp()
+        {
+            Debug.Log("Charge State: Mouse Up");
+            ChangeState?.Invoke(this, ReleaseState.Instance);
+        }
+
+        public override void OnKeyPressed(KeyCode keyCode)
+        {
+            switch (keyCode)
+            {
+                case KeyCode.Escape:
+                {
+                    Debug.Log("Charge State: Escape key pressed");
+                    ChangeState?.Invoke(this, IdleState.Instance);
+                    break;
+                }
+            }
+        }
+
+        private ChargeState()
+        {
+
+        }
+    }
+
+    private class ReleaseState : State
+    {
+        private static ReleaseState instance;
+        public static ReleaseState Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ReleaseState();
+                }
+
+                return instance;
+            }
+        }
+
+        public override event EventHandler<State> ChangeState;
+
+        public override void OnEnter()
+        {
+            Debug.Log("Entering Release State");
+            ChangeState?.Invoke(this, IdleState.Instance);
+        }
+
+        public override void OnExit()
+        {
+            Debug.Log("Exiting Release State");
+        }
+        public override void OnMouseDown()
+        {
+            Debug.Log("Release State: Mouse Down");
+        }
+
+        public override void OnMouseUp()
+        {
+        }
+
+        public override void OnKeyPressed(KeyCode keyCode)
+        {
+        }
+
+        private ReleaseState()
+        {
+
+        }
+    }
+
 }
